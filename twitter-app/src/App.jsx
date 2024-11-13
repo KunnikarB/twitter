c880750196dc9db1936be294b733e6277714d5ff
kunnikarB <kunnikarboon@gmail.com>
2024-11-13 13:15:17 +0100

    Created Home component, added dummy data and updated routes at App.jsx

--- twitter-app/src/App.jsx
+++ twitter-app/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartPage from './pages/StartPage';
+import Home from './pages/Home';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        
+        <Route path="/home" element={<Home />} />
         <Route path="/" element={<StartPage />} />
       </Routes>
     </Router>
