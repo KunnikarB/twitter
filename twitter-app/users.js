d8d838010a62d10cb3eb90b22ee65a7853fb6fd6
kunnikarB <kunnikarboon@gmail.com>
2024-11-13 13:00:10 +0100

    Added user demo, created storage utility and installed styled-conponents dependencies

---
+++ twitter-app/users.js
@@ -0,0 +1,51 @@
+export const users = [
+  {
+    id: '1',
+    name: 'Kunnikar ',
+    image: '/avatars/girl2.png',
+    bio: 'Just here, doing my thing. I love #react and #javascript',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.chCkD_cowFTdSauUP1iprlX-uWvCQ6AuBOYcnNrFyxw',
+  },
+  {
+    id: '2',
+    name: 'Thomas',
+    image: '/avatars/boy2.png',
+    bio: 'Deploy activity feeds and chat at scale with Stream',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMiJ9.TLJmATSD5UTtB3_oSN3XYLd6IA1XwuNvQw6YsYBx4dY',
+  },
+  {
+    id: '3',
+    name: 'Mary',
+    image: '/avatars/girl1.png',
+    bio: 'Just Jake, nothing much',
+
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMyJ9.9O1HmpZ_6JE8nzwcQ5ZyyehIPtJx4l7Dvk8YsJo-L_Y',
+  },
+  {
+    id: '4',
+    name: 'Hanks',
+    image: '/avatars/boy3.png',
+    bio: 'How are you?',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNCJ9.UNPiltc3AMcDOMEPakIo3I4zsTaz2J4IER9SDlXj-sk',
+  },
+  {
+    id: '5',
+    name: 'Daisy',
+    image: '/avatars/girl3.png',
+    bio: 'I am Daisy here. I do things on #react and #javascript',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNSJ9.JrW3NA5577KHmGQIipBOLYXYY551a7l9atI3CeLdxE4',
+  },
+  {
+    id: '6',
+    name: 'Jake',
+    image: '/avatars/boy1.png',
+    bio: 'Just Jake, nothing much',
+    token:
+      ' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNiJ9.EcS2JmGzAHVqdSyCeCeF_eBvvUuXjlIwH75oYYqL2Eg',
+  },
+];
