26802928f6bd11257a15559700f95bb636db2d27
kunnikarB <kunnikarboon@gmail.com>
2024-11-20 17:00:35 +0100

    modified avatas

---
+++ twitter-app/src/components/Card.jsx
@@ -0,0 +1,15 @@
+
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="https://via.placeholder.com/150" alt="profile" />
+      <div>
+        <h3>John Doe</h3>
+        <p>john_doe</p>
+     </div>
+    </div>
+  );
+}
+
+export default Card;
