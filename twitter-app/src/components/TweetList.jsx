82eeeae2f4555cc798ea2e8dd816fe25273510da
kunnikarB <kunnikarboon@gmail.com>
2024-11-20 20:51:02 +0100

    updated user profile

--- twitter-app/src/components/TweetList.jsx
+++ twitter-app/src/components/TweetList.jsx
@@ -2,8 +2,7 @@ import PropTypes from 'prop-types';
 import TweetInteractions from './TweetInteractions';
 import { TweetContext } from '../context/TweetContext';
 import { useContext } from 'react';
-import profileImg from '../../public/avatars/girl2.png';
-import { users } from '../users';
+import UserProfile from './UserProfile';
 
 const TweetList = () => {
   const { tweets } = useContext(TweetContext);
@@ -12,14 +11,10 @@ const TweetList = () => {
     <ul>
       {tweets.map((tweet) => (
         <div className="tweet" key={tweet.id}>
-
           <div className="post">
-            <div className="user-post">
-              <img className="post-logo" src={profileImg} alt="{users[0].name}" />
-              <small>Post by {users[0].name}</small>
-            </div>
-            <p>{tweet.text}</p>
-
+             <UserProfile user={tweet.user} />
+              <p>{tweet.text}</p>
+              
             <div className="tweet-interactions">
               <p>Likes: {tweet.likes}</p>
               <p>Retweets: {tweet.retweets}</p>
@@ -29,7 +24,7 @@ const TweetList = () => {
             <TweetInteractions tweetId={tweet.id} />
             <div className="replies">
               {tweet.replies.map((reply) => (
-                <div key={reply.id} className='reply'>
+                <div key={reply.id} className="reply">
                   <p>💬 {reply.text}</p>
                 </div>
               ))}
