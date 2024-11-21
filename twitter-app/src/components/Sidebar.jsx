500189b764b2d33056a18fefce1e5b0ee3435453
kunnikarB <kunnikarboon@gmail.com>
2024-11-21 06:34:25 +0100

    Update Sidebar and Added Logout function

--- twitter-app/src/components/Sidebar.jsx
+++ twitter-app/src/components/Sidebar.jsx
@@ -4,39 +4,55 @@ import { IoNotifications } from 'react-icons/io5';
 import { FaUser } from 'react-icons/fa';
 import UserProfile from './UserProfile';
 import '../index.css';
+import { useUserContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
   const style = {
     marginRight: '10px',
     fontSize: '20px',
   };
-  return (
-    <div className='sidebar-container'>
-    <div className='sidebar-top'>
-      <div className="logo">
-        <Xsvg />
-      </div>
-      <div className="sidebar">
-        <ul>
-          <li>
-            <MdHomeFilled style={style} />
-            Home
-          </li>
 
-          <li>
-            <IoNotifications style={style} />
-            Notifications
-          </li>
-          <li>
-            <FaUser style={style} />
-            Profile
-          </li>
-        </ul>
+  
+  const navigate = useNavigate();
+  const { setCurrentUser } = useUserContext();
+
+  const Logout = () => {
+    setCurrentUser(null); // Clear the current user
+    navigate('/'); 
+  };
+
+
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar-top">
+        <div className="logo">
+          <Xsvg />
+        </div>
+        <div className="sidebar">
+          <ul>
+            <li>
+              <MdHomeFilled style={style} />
+              Home
+            </li>
+
+            <li>
+              <IoNotifications style={style} />
+              Notifications
+            </li>
+            <li>
+              <FaUser style={style} />
+              Profile
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div className='sidebar-user'>
-      <UserProfile />
-    </div>
+      <div className="sidebar-user">
+        <div className='user-logout'>
+          <UserProfile />
+          <button onClick={Logout} className='btn'>Logout</button>
+        </div>
+      </div>
     </div>
   );
 };
